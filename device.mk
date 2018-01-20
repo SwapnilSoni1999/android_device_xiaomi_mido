5addaf90d57377ad28e12cb6719f6579d3b2c0f6
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2018-01-20 16:53:49 +0000

    mido: add missing flags

--- device.mk
+++ device.mk
@@ -25,6 +25,13 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# TARGET_BOARD_PLATFORM specific featurization
+QCOM_BOARD_PLATFORMS := msm8953
+
+# List of targets that use video hw
+MSM_VIDC_TARGET_LIST := msm8953
+
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
